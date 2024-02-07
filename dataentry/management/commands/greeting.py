8ec82d7589e4f2a8afcca0afb2bcd35a789b0be0
rajeshchaudhary007 <rajeshchaudhary24478546@gmail.com>
2024-02-07 21:16:17 +0545

    initial commit

---
+++ dataentry/management/commands/greeting.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandParser
+
+
+
+
+
+class Command(BaseCommand):
+    help = 'Greets the user'
+    
+    
+    def add_arguments(self, parser):
+        parser.add_argument('name',type=str,help='Specifies user name')
+    
+    def handle(self,*args, **kwargs):
+        name = kwargs['name']
+        greeting = f'Hi {name},Good Morning!'
+        self.stdout.write(self.style.SUCCESS(greeting))
